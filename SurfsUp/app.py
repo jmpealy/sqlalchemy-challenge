39e035051e76002138d16e13968fd48fbf35f050
jmpealy <jmpealy@gmail.com>
2023-07-17 22:47:58 -0500

    Worked on/finished Flask app, added more links

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,21 +1,23 @@
 # Import the dependencies.
 import os
 import sqlalchemy
+
+
+import datetime as dt
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
-import datetime as dt
-
+from sqlalchemy import create_engine, exc
+from sqlalchemy import func
 from flask import Flask, jsonify
 
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///../Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
-# reflect an existing database into a new model
+# Reflect an existing database into a new model
 Base = automap_base()
-# reflect the tables
+# Reflect the tables
 Base.prepare(autoload_with=engine)
 
 # Save references to each table
@@ -39,13 +41,13 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/passengers<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/<start><br/>"
-        f"/api/v1.0/<start>/<end>"
+        f"Precipitation: /api/v1.0/precipitation<br/>"
+        f"List of Stations: /api/v1.0/stations<br/>"
+        f"Temperature for one year: /api/v1.0/tobs<br/>"
+        f"Temperature stat from the start date(yyyy-mm-dd): /api/v1.0/yyyy-mm-dd<br/>"
+        f"Temperature stat from start to end dates(yyyy-mm-dd): /api/v1.0/yyyy-mm-dd/yyyy-mm-dd"
     )
-    #Convert the query results from the precipitation analysis (i.e. retrieve only the last 12 months of data) to a dictionary using date as the key and prcp as the value.
+    #Convert the query results from the precipitation analysis (one year's worth of data) to a dictionary using date as the key and prcp as the value.
 @app.route("/api/v1.0/precipitation")
 def precipitation():
     # Create our session (link) from Python to the DB
@@ -56,8 +58,104 @@ def precipitation():
     latest_date = dt.datetime.strptime(last_date[0], '%Y-%m-%d')
     # Calculate the date one year from the last date in data set.
     one_year = dt.date(latest_date.year - 1, latest_date.month, latest_date.day)
-    one_year
+ 
     # # Perform a query to retrieve the data and precipitation scores
     sel = [Measurements.date, Measurements.prcp]
     results = session.query(*sel).filter(Measurements.date >= one_year).all()
-    results
+
+    #Create a dictionary of precipitation results
+    precip = []
+    for date, prcp in results:
+        prcp_dict = {}
+        prcp_dict['Date'] = date
+        prcp_dict['Precipitation'] = prcp
+        precip.append(prcp_dict)
+    
+    return jsonify(precip)
+
+    #Create a query of all the station data
+@app.route('/api/v1.0/stations')
+def stations():
+    session = Session(engine)
+    sel = [Stations.station, Stations.name, Stations.latitude, Stations.longitude, Stations.elevation]
+    queryresult = session.query(*sel).all()
+    session.close()
+
+    #Organize the station data into a dictionary
+    stations = []
+    for station, name, lat, lon, elev in queryresult:
+        station_dict = {}
+        station_dict["Station"] = station
+        station_dict["Name"] = name
+        station_dict["Lat"] = lat
+        station_dict["Lon"] = lon
+        station_dict["Elevation"] = elev
+        stations.append(station_dict)
+
+    #Return a JSON list of all the stations
+    return jsonify(stations)
+
+    #Query the dates and temperature observations of the most-active station for the previous year of data.
+@app.route('/api/v1.0/tobs')
+def tobs():
+    session = Session(engine)
+    lateststr = session.query(Measurements.date).order_by(Measurements.date.desc()).first()[0]
+    latestdate = dt.datetime.strptime(lateststr, '%Y-%m-%d')
+    querydate = dt.date(latestdate.year -1, latestdate.month, latestdate.day)
+    sel = [Measurements.date, Measurements.tobs]
+    queryresult = session.query(*sel).filter(Measurements.date >= querydate).all()
+    session.close()
+
+    #Organize the data into a dictionary
+    tobsall = []
+    for date, tobs in queryresult:
+        tobs_dict = {}
+        tobs_dict["Date"] = date
+        tobs_dict["Tobs"] = tobs
+        tobsall.append(tobs_dict)
+
+    #Return a JSON list of temperature observations for the previous year.
+    return jsonify(tobsall)
+
+    #Create a query returning TOBS stats for a period starting from a certain date until the end of the dataset
+@app.route('/api/v1.0/<start>')
+def get_tobs_start(start):
+    session = Session(engine)
+    queryresult = session.query(func.min(Measurements.tobs), func.avg(Measurements.tobs), func.max(Measurements.tobs)).\
+        filter(Measurements.date >= start).all()
+    session.close()
+
+    #Organize the data into a dictionary
+    tobsall = []
+    for min, avg, max in queryresult:
+        tobs_dict = {}
+        tobs_dict["Min"] = min
+        tobs_dict["Max"] = max
+        tobs_dict["Average"] = avg
+        tobsall.append(tobs_dict)
+
+    #Return a JSON list of the TOBS stats for a specified start range
+    return jsonify(tobsall)
+
+    #Create a query returning TOBS stats for a period between two defined dates
+@app.route('/api/v1.0/<start>/<stop>')
+def get_tobs_start_stop(start, stop):
+    session = Session(engine)
+    queryresult = session.query(func.min(Measurements.tobs), func.avg(Measurements.tobs), func.max(Measurements.tobs)).\
+        filter(Measurements.date >= start).filter(Measurements.date <= stop).all()
+    session.close()
+
+    #Organize the data into a dictionary
+    tobsall = []
+    for min, avg, max in queryresult:
+        tobs_dict = {}
+        tobs_dict["Min"] = min
+        tobs_dict["Max"] = max
+        tobs_dict["Average"] = avg
+        tobsall.append(tobs_dict)
+
+    #Return a JSON list of the TOBS stats for a specified start-end range
+    return jsonify(tobsall)
+
+if __name__=='__main__':
+    app.run(debug=True)
---
+++ Useful links.md
@@ -0,0 +1,18 @@
+# sqlalchemy-challenge
+
+#https://sparkbyexamples.com/pandas/calculate-summary-statistics-in-pandas/?expand_article=1
+I used this for summary statistics in the climate_starter jupyter notebook
+
+#https://www.statology.org/pandas-plot-axis-labels/
+I needed reminding how to amend/add x and y labels for a matplotlib histogram plot
+
+#https://stackoverflow.com/questions/45273731/binning-a-column-with-pandas
+I used this to remind myself how to take advantage of built-in 'binning' with Pandas
+
+#https://flask.palletsprojects.com/en/2.3.x/api/
+I used this as a Flask reference guide given we only spent one class on Flask
+
+
+
+
+
